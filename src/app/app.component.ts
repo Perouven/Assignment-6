e32066d4a480309c422ecf8ad5b1f68dc6e73270
Perouven <vperouven@gmail.com>
2020-08-13 07:31:45 +0400

    assignment declaration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB422 – Assignment 05
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+*  assignment has been copied manually or electronically from any other source (including web sites) or 
+*  distributed to other students.
+* 
+*  Name: Perouven Vencadachellum   Student ID:120960182  Date:12 August 2020
+*  Online link: https://musing-lumiere-6a9e45.netlify.app
+*
+********************************************************************************/ 
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
